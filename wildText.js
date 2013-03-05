80df44d8a37a7538d667931125f905983aedfc66
grantges <bert@grantges.co>
2013-03-05 17:26:10 -0600

    Update wildText.js
    has been updated to:
    
    1. Use the TMP directory instead of Application Data Directory
    2. Check to see if the image already exists or not.

--- wildText.js
+++ wildText.js
@@ -1,8 +1,8 @@
 /*
 WildText CommonJS module for Appcelerator Titanium
-
+ 
 Create dynamic gradient filled text in your iOS Applications using this simple module.
-
+ 
 @params :   text 	- String. The text for your label
 			font 	- Font.  Specify the font attributes for the label (defaults to standard size, weight and family),
 			backgroundGradient - BackgroundGradient. Specify your backgroundGradient object (defaults to White to Black linear gradient),
@@ -12,9 +12,9 @@ Create dynamic gradient filled text in your iOS Applications using this simple m
 			Right	- Integer. Right Property for your Label
 			
 Example Usage:
-
+ 
 var WildText = require("wildText");
-
+ 
 var win = Ti.UI.createWindow();
 var text = new WildText({
 	text: "Custom Gradient and Font",
@@ -34,60 +34,62 @@ var text = new WildText({
 	},
 	top: 10,
 });
-
+ 
 win.add(text);
 win.open();
-
+ 
 */
-
-
+ 
+ 
 var WildText = function(_args){
 	
-	_arg = _args || {};
-		
-	// Create an image from the Label to get the masked Text
-	this.label = Ti.UI.createLabel({
-		text: _args.text,
-		color: "#000",
-		font: _args.font || {
-			fontSize: "24dp",
-			fontFamily: "Calibri",
-			fontWeight: "bold"
-		},
-		backgroundColor: "transparent",
-		width: Ti.UI.SIZE,
-		height: Ti.UI.SIZE
-	}).toImage();
+	_args = _args || {};
 	
-	this.background = Ti.UI.createView({
-		width: this.label.width, 
-		height: this.label.height,
-		backgroundGradient: _args.backgroundGradient || {
-			type: 'linear',
-			startPoint:{x:0, y:0},
-			endPoint: {x:0, y:"100%"},
-			colors:[
-				{color: "#fff", offset:0.0},
-				{color: "#000", offset:1.0}
-			],
-		}
-	}).toImage();
-	
-	var FOLDER_PATH = Ti.Filesystem.applicationDataDirectory; 
+	var FOLDER_PATH = Ti.Filesystem.tempDirectory;
 	var filename = _args.text.replace(" ", "_");
-	Ti.API.info(filename);
 	
-	this.labelImageFile = Ti.Filesystem.getFile(FOLDER_PATH, filename+"_text.png");
-	if(this.labelImageFile){
+	this.labelImageFile = Ti.Filesystem.getFile(FOLDER_PATH, filename + "_text.png");
+	if (this.labelImageFile.exists()) {
+		Ti.API.info("Reading " + this.labelImageFile.nativePath);
+		this.label = this.labelImageFile.read();
+	} else {
+		// Create an image from the Label to get the masked Text
+		this.label = Ti.UI.createLabel({
+			text: _args.text,
+			color: "#000",
+			font: _args.font || {
+				fontSize: "24dp",
+				fontFamily: "Calibri",
+				fontWeight: "bold"
+			},
+			backgroundColor: "transparent",
+			width: Ti.UI.SIZE,
+			height: Ti.UI.SIZE
+		}).toImage();
+		Ti.API.info("Writing " + this.labelImageFile.nativePath);
 		this.labelImageFile.write(this.label);
 	}
 	
-	
-	this.bkgImageFile = Ti.Filesystem.getFile(FOLDER_PATH, filename+"_bkg.png");
-	if(this.bkgImageFile){
+	this.bkgImageFile = Ti.Filesystem.getFile(FOLDER_PATH, filename + "_bkg.png");
+	if (!this.bkgImageFile.exists()) {
+		this.background = Ti.UI.createView({
+			width: this.label.width, 
+			height: this.label.height,
+			backgroundGradient: _args.backgroundGradient || {
+				type: 'linear',
+				startPoint:{x:0, y:0},
+				endPoint: {x:0, y:"100%"},
+				colors:[
+					{color: "#fff", offset:0.0},
+					{color: "#000", offset:1.0}
+				],
+			}
+		}).toImage();
+		
+		Ti.API.info("Writing " + this.bkgImageFile.nativePath);
 		this.bkgImageFile.write(this.background);
 	}
-
+ 
 	var API = Titanium.UI.createMaskedImage({
 	    image : this.bkgImageFile.nativePath , // alpha mask
 	    mask : this.labelImageFile.nativePath, // image to mask
